5978e3074e6d219e7ffc4e928e3573292f37552e
keshavalt22 <keshavkhatri35@gmail.com>
2022-01-11 16:43:12 +0530

    assignment

--- block-BJaaew/code/script.js
+++ block-BJaaew/code/script.js
@@ -1,20 +1,58 @@
 let rootElement = document.querySelector("ul");
+let rootBtn = document.querySelector(".btn"); 
+let search = document.querySelector(".search");
+let allPeople = got.houses.reduce((acc,cv) => {
+    acc = acc.concat(cv.people);
+    return acc;
+}, []);
 
-got.houses.forEach((book) => {
-    let li = document.createElement("li");
-    let img = document.createElement("img");
-    img.src = book.image;
-    img.alt = book.title;
-    let h2 = document.createElement("h2")
-    h2.innerText = book.title  
-    let p = document.createElement("p");
-    p.innerHTML = "Author:";
-    let span = document.createElement("span");
-    let authorName = document.createTextNode(book.author);
-    span.append(authorName);
-    p.append(span);
-    let button = document.createElement("button");
-    button.innerText = "Buy Now";
-    li.append(img,h2,p,button);
-    rootElement.append(li);
-})
+let allBtn = got.houses.map((house) => house.name);
+
+let activeHouse = "";
+
+function createCardsUI(data = []) {
+    rootElement.innerText = "";
+    data.forEach((people) => {
+        let li = document.createElement("li");
+        let img = document.createElement("img");
+        img.src = people.image;
+        img.alt = people.name;
+        let h2 = document.createElement("h2");
+        h2.innerText = people.name;  
+        let p = document.createElement("p");
+        p.innerHTML = people.description;
+        let button = document.createElement("button");
+        button.innerText = "Know More"
+        li.append(img,h2,p,button);
+        rootElement.append(li);
+    });
+}
+function createBtnUI(btn = []) {
+    rootBtn.innerText = "";
+    btn.forEach((tag) => {
+        let button = document.createElement("button")
+        button.innerText = tag;
+        if(activeHouse === tag){
+            button.classList.add(`selected`);
+        }
+        button.addEventListener("click", () => {
+            activeHouse = tag;
+            let peopleOfTheHouse = got.houses.find((house) => house.name === tag).people || [];
+            createCardsUI(peopleOfTheHouse);
+            createBtnUI(allBtn);
+        })
+        rootBtn.append(button);
+    });
+}
+
+function handleSearch(event) {
+    let searchText = event.target.value;
+    let filteredPeople = allPeople.filter((people) => people.name.toLowerCase().includes(searchText.toLowerCase()));
+    createCardsUI(filteredPeople);
+
+}
+
+search.addEventListener("input",handleSearch);
+
+createCardsUI(allPeople);
+createBtnUI(allBtn);
